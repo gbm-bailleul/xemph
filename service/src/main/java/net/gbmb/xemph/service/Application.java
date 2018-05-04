23612df39f9c6ff5e40d031c2199c46ed48f6a03
Guillaume Bailleul <gbm.bailleul@gmail.com>
2018-05-04 21:22:59 +0200

    Simplification of jackson mapper configuration

--- service/src/main/java/net/gbmb/xemph/service/Application.java
+++ service/src/main/java/net/gbmb/xemph/service/Application.java
@@ -1,7 +1,6 @@
 package net.gbmb.xemph.service;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.ObjectWriter;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import net.gbmb.xemph.Packet;
 import net.gbmb.xemph.xml.XmpReader;
@@ -11,6 +10,7 @@ import org.apache.pdfbox.pdfparser.PDFParser;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.*;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.*;
 import org.springframework.web.bind.annotation.*;
 
@@ -24,6 +24,13 @@ import java.io.InputStream;
 @Controller
 @EnableAutoConfiguration
 public class Application {
+
+    @Bean
+    public ObjectMapper objectMapper () {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS,false);
+        return mapper;
+    }
 
     @RequestMapping("/")
     @ResponseBody
@@ -41,7 +48,7 @@ public class Application {
             produces = "application/json",
             method = RequestMethod.POST)
     @ResponseBody
-    String extract (HttpServletRequest request) throws IOException {
+    ExtractionResult extract (HttpServletRequest request) throws IOException {
         // copy in tmp (TODO very ugly)
         File tmp = File.createTempFile("xemph-","-service");
         FileOutputStream fos = new FileOutputStream(tmp);
@@ -55,19 +62,11 @@ public class Application {
         PDDocument document = parser.getPDDocument();
         InputStream xmp = document.getDocumentCatalog().getMetadata().exportXMPMetadata();
         XmpReader xmpReader = new XmpReader();
-        ExtractionResult result = null;
         try {
             Packet packet = xmpReader.parse(xmp);
-            result = new ExtractionResult(packet);
+            return new ExtractionResult(packet);
         } catch (XMLStreamException e) {
-            result = new ExtractionResult(e);
+            return new ExtractionResult(e);
         }
-        // return packet
-        ObjectMapper mapper = new ObjectMapper();
-        ObjectWriter writer =mapper.writer();
-        String jsonInString = writer
-                .without(SerializationFeature.FAIL_ON_EMPTY_BEANS)
-                .writeValueAsString(result);
-        return jsonInString;
     }
 }
